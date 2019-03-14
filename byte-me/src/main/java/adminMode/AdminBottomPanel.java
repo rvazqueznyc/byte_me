ef3bbfd2345db14a2fbf4cca52e831a85420534b
rvazqueznyc <46765660+rvazqueznyc@users.noreply.github.com>
2019-03-14 14:01:11 -0400

    Add Button Added

--- byte-me/src/main/java/adminMode/AdminBottomPanel.java
+++ byte-me/src/main/java/adminMode/AdminBottomPanel.java
@@ -35,6 +35,96 @@ public class AdminBottomPanel extends JPanel {
 		filesIndexed = new JLabel("Number of files indexed: " /*needs reference*/);
 		versionNumber = new JLabel("Search Engine version: " /*needs reference*/);
 		
+		
+		
+		
+		// add button action listener 
+		addButton.addActionListener(new ActionListener() 
+		
+		{
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			
+		// here is frame in which select file window opens
+		Component frame = null;
+		
+		//  opens the select window 
+		if(filechoose.showSaveDialog(frame)==JFileChooser.APPROVE_OPTION);
+		
+		// it picks the file path we choose
+		File f=	filechoose.getSelectedFile();
+
+		
+		
+		// Here we check that the file we have picked up is already stored or not
+		
+		
+		int test=0;
+		
+		// reading all lines of output.txt files and match them with the file we have selected 
+           BufferedReader reader;
+		
+		try {
+			reader = new BufferedReader(new FileReader(
+					"output.txt"));
+			String line = reader.readLine();
+			
+			
+			
+			while (line != null) {
+				// if file is there set test variable value to 1
+				if(line.equals(f.getAbsolutePath()))
+				{
+				
+					test=1;
+				}
+				
+				
+				// read next line
+				line = reader.readLine();
+			
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		// if test is equal to 1 means file is already in table we don't need to rewrite it
+		
+		if(test==1)
+		{// Show pop up window that already inserted
+			JOptionPane.showMessageDialog(frame, "Already Inserted");
+		}
+		else
+		{
+			// else just write the file path to the output.txt
+		
+			
+			// here is a try and catch block for the output.txt file
+// try to open this file
+			try(FileWriter fw = new FileWriter("output.txt", true);
+			   // Pass this file writer to butter writer
+				BufferedWriter bw = new BufferedWriter(fw);
+			    // then pass this buffer writer to PrintWriter so we can write in the file line by line
+					PrintWriter out = new PrintWriter(bw))
+			{
+				// this will write in the file the path of our selected file
+			    out.println(f.getAbsolutePath());
+			   
+			    // Here we are adding this file path as row to the model of table so it show up in table 
+			    AdminCenterPanel.model.addRow(new Object[]{f.getAbsolutePath(),"Indexed"});
+			   
+			  
+			} catch (IOException e) {
+			    //exception handling left as an exercise for the reader
+				// this will show pop up about file not found
+				JOptionPane.showMessageDialog(frame, "File Not Found");
+			}
+		  }
+		}});
+		
 		//Setting the grid layout and positioning components
 		setLayout(new GridBagLayout());
 		
@@ -76,16 +166,5 @@ public class AdminBottomPanel extends JPanel {
 		gc.anchor = GridBagConstraints.CENTER;
 		add(versionNumber, gc);
 		
-		addButton.addActionListener(new ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent e) {
-				JFileChooser fc = new JFileChooser();
-				fc.setCurrentDirectory(new java.io.File("."));
-				fc.setDialogTitle("Add a file to the index");
-				
-				if(fc.showOpenDialog(addButton) == JFileChooser.APPROVE_OPTION) {
-					AdminCenterPanel.fileChosen = (fc.getSelectedFile().getAbsolutePath());
-				}
-			}
-		});
 	}
 }
